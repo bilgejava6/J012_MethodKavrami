02527d163be76fe433a952ba7765d56bec220bb5
bilgejava6 <bilgejava6@gmail.com>
2024-11-02 14:19:39 +0300

    Öğrenci diploma örneği ile boolean array kullanımı

---
+++ src/main/java/com/muhammet/Runner_BooleanArray_Ornek.java
@@ -0,0 +1,63 @@
+package com.muhammet;
+
+public class Runner_BooleanArray_Ornek {
+    static  boolean[] ogrenciDurumu = new boolean[20];
+    static  String[] ogrenciListesi = {
+            "Ali Yılmaz",
+            "Ayşe Demir",
+            "Mehmet Öz",
+            "Fatma Kara",
+            "Ahmet Çelik",
+            "Elif Aksoy",
+            "Mustafa Şahin",
+            "Zeynep Güneş",
+            "Hüseyin Koç",
+            "Emine Taş",
+            "Canan Kaya",
+            "İbrahim Arslan",
+            "Selin Bulut",
+            "Oğuz Polat",
+            "Sevgi Aydın",
+            "Eren Şen",
+            "Yasemin Çiçek",
+            "Barış Deniz",
+            "Gökhan Kılıç",
+            "Nuray Doğan"
+    };
+    int[][] notlar = {
+            {85, 90},  // 1. öğrenci
+            {78, 82},  // 2. öğrenci
+            {2, 88},  // 3. öğrenci
+            {74, 76},  // 4. öğrenci
+            {88, 91},  // 5. öğrenci
+            {67, 70},  // 6. öğrenci
+            {90, 5},  // 7. öğrenci
+            {83, 85},  // 8. öğrenci
+            {76, 80},  // 9. öğrenci
+            {89, 92},  // 10. öğrenci
+            {81, 84},  // 11. öğrenci
+            {7, 75},  // 12. öğrenci
+            {91, 93},  // 13. öğrenci
+            {68, 4},  // 14. öğrenci
+            {77, 79},  // 15. öğrenci
+            {86, 87},  // 16. öğrenci
+            {93, 90},  // 17. öğrenci
+            {72, 78},  // 18. öğrenci
+            {4, 6},  // 19. öğrenci
+            {88, 89}   // 20. öğrenci
+    };
+
+    public static void main(String[] args) {
+        /**
+         * bir okulda öğrencilerin yıl sonu notları verilmiş ve okul u geçen
+         * geçemeyen öğrencilere göre diplomaları dağıtılacaktır.
+         * Aşağıda öğrencilerin notları otomatik olarak belirlenmiş ve
+         * bir liste içerisine eklenmiştir.
+         * Buna göre öğrencilerin geçti/kaldı durumuna göre karne alıp
+         * alamayacaklarını bulan bir kodlama yapınız.
+          */
+
+
+    }// end main method
+
+}//end class
